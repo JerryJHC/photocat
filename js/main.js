ea767c49cb2eb551710fb8c80505a21f2e963b3b
Jerry Henríquez <jerryhenriquez@gmail.com>
2019-01-30 17:27:50 +0100

    agregado ejemplo con get de imagenes y posterior generacion de img con las imagenes obtenidas

--- js/main.js
+++ js/main.js
@@ -1,26 +1,45 @@
-function ajax_get(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log('responseText:' + xmlhttp.responseText);
-            try {
-                var data = JSON.parse(xmlhttp.responseText);
-            } catch (err) {
-                console.log(err.message + " in " + xmlhttp.responseText);
-                return;
-            }
-            callback(data);
-        }
+function ajax(method, url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
     };
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
 }
 
-ajax_get('https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc', function (data) {
-    document.getElementById("id").innerHTML = data[0]["id"];
-    document.getElementById("url").innerHTML = data[0]["url"];
+function showCats(data) {
+  try {
+    const images = document.getElementById("images");
+    let cats = JSON.parse(data);
+    cats.forEach(element => {
+      let img = document.createElement("img");
+      img.src = element.url;
+      images.appendChild(img);
+    });
+  } catch{
+    console.log("Error en parse");
+  }
+}
 
-    var html = '<img src="' + data[0]["url"] + '">';
-    document.getElementById("image").innerHTML = html;
-});
+window.onload = () => {
+  ajax('GET', 'https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc')
+    .then(showCats)
+    .catch((error) => {
+      console.log("Error" + error.statusText + " - " + error.status);
+    });
+}
